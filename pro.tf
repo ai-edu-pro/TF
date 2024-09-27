2fede90bc809dde97569ac639795861f9a972a6c
kim Young Chul (김영철) <163226824+powermvp116@users.noreply.github.com>
2024-09-27 16:08:10 +0900

    Add files via upload

---
+++ pro.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "region" {
+  type        = string
+  description = "region name"
+  default     = "ap-northeast-2"
